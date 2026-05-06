002280e7428642988de82f7619e42d936de3016c
XUE_Jintao <thecaichicken@gmail.com>
2026-05-06 19:58:22 +0800

    algo rule based

--- source/algo/multiagent/hc_factory/rule_based.py
+++ source/algo/multiagent/hc_factory/rule_based.py
@@ -1,29 +1,43 @@
-from __future__ import annotations
+# -*- coding: utf-8 -*-
+from __future__ import division
+import os
+import numpy as np
+import torch
+from torch import optim
+from torch.nn.utils import clip_grad_norm_
+from torch import nn
+
+from rl_games.common import vecenv
+# from ..utils import vecenv
+from rl_games.algos_torch import torch_ext
 
 
-class ProductSequencingAgent:
-    """
-    Product Sequencing Agent
 
-    This agent is responsible for determining the optimal order in which products should be manufactured,
-    based on the current production order and the state of the environment/assets.
+class RuleBasedAgent():
+    def __init__(self, base_name, params):
+        config = params['config']
+        self.env_config = config.get('env_config', {})
+        self.num_actors = config.get('num_actors', 1)
+        self.env_name = config['env_name']
+        print("Env name:", self.env_name)
+        self.env_info = config.get('env_info')
+        if self.env_info is None:
+            self.vec_env = vecenv.create_vec_env(self.env_name, self.num_actors, **self.env_config)
+            self.env_info = self.vec_env.get_env_info()
 
-    Inputs:
-        - production_order (list[dict]): List containing product information and requirements to be produced.
-        - env_state_action_dict (dict): Current state and actions of all relevant assets in the environment
-          (e.g., humans, robots, machines, storage systems).
+    
+    
+    def play_steps(self):
+        while True:
+            obs : dict = self.vec_env.reset()
+            with torch.no_grad():
+                next_obs, rewards, dones, infos, action = self.vec_env.step(action)
+            return next_obs, rewards, dones, infos, action
 
-    Outputs:
-        - The next product (or list of products) to be prioritized for production. 
-        - Or the schedule of products to be produced in the future.
-    """
 
-    def reset(self, production_order: list[dict], env_state_action_dict: dict) -> list[dict]:
-
-        # For now, return the production order unchanged.
-        return production_order
-
-    def step(self, production_order: list[dict], env_state_action_dict: dict) -> list[dict]:
-
-        # Placeholder: Returns production order unchanged.
-        return production_order
+    def train(self):
+        self.vec_env.reset()
+        while True:
+            action = None
+            action_extra = {}
+            self.vec_env.step(action, action_extra)
